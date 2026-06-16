88d1e95c6c829834e707f24084bcbfbde9ef49b0
Vetusto79 <ipozdnyakov@mail.ru>
2026-06-17 00:08:15 +0300

    Create page.tsx

---
+++ app/articles/potolochnye-sistemy/page.tsx
@@ -0,0 +1,19 @@
+import ArticleCategoryPage from "../../components/ArticleCategoryPage";
+import { getCategoryBySlug } from "../../data/articles";
+
+const category = getCategoryBySlug("potolochnye-sistemy");
+
+export const metadata = {
+  title: category?.seoTitle || "Статьи Иделеон",
+  description:
+    category?.seoDescription ||
+    "Полезные статьи Иделеон о строительных материалах и комплектации объектов.",
+};
+
+export default function CategoryPage() {
+  if (!category) {
+    return null;
+  }
+
+  return <ArticleCategoryPage category={category} />;
+}
